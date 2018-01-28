f5eba5660bc57799db5e91485d0bc74ca64c41b4
derek <derek@llamabagel.ca>
2018-01-28 00:16:22 -0500

    Added the other required API methods

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -22,7 +22,7 @@ namespace ARDrawServer.Controllers
         }
         
         // GET api/values
-        [HttpGet]
+        [HttpGet("region")]
         public IActionResult Get()
         {
             var drawings = new List<Drawing>();
@@ -33,7 +33,7 @@ namespace ARDrawServer.Controllers
             {
                 while (reader.Read())
                 {
-                    var drawing = new Drawing()
+                    var drawing = new Drawing
                     {
                         Id = reader.GetInt32(0),
                         Latitude = reader.GetDecimal(1),
@@ -47,33 +47,63 @@ namespace ARDrawServer.Controllers
                     drawings.Add(drawing);
                 }
             }
+            
+            _conn.Close();
 
             return Ok(drawings);
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet]
+        public IActionResult Regional([FromQuery] decimal lat, [FromQuery] decimal lon)
         {
-            return "value";
+            var drawings = new List<Drawing>();
+            _conn.Open();
+            
+            var command = new MySqlCommand("SELECT * FROM drawings WHERE latitude >= @lat - 0.05 AND longitude >= @lon - 0.05 AND latitude <= @lat + 0.05 AND longitude <= @long + 0.05", _conn);
+            command.Parameters.Add(new MySqlParameter("lat", lat));
+            command.Parameters.Add(new MySqlParameter("lon", lon));
+            
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var drawing = new Drawing
+                    {
+                        Id = reader.GetInt32(0),
+                        Latitude = reader.GetDecimal(1),
+                        Longitude = reader.GetDecimal(2),
+                        Altitude = reader.GetDecimal(3),
+                        Bearing = reader.GetDecimal(4),
+                        Color = reader.GetInt32(6),
+                        PathData = reader.GetString(5)
+                    };
+                    
+                    drawings.Add(drawing);
+                }
+            }
+            
+            _conn.Close();
+
+            return Ok(drawings);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Drawing value)
         {
-        }
+            _conn.Open();
+            
+            var command = new MySqlCommand("INSERT INTO drawings(latitude, longitude, altitude, bearing, pathData, color) VALUES @lat, @lon, @altitude, @bearing, @path, @color", _conn);
+            command.Parameters.Add(new MySqlParameter("lat", value.Latitude));
+            command.Parameters.Add(new MySqlParameter("lon", value.Longitude));
+            command.Parameters.Add(new MySqlParameter("altitude", value.Altitude));
+            command.Parameters.Add(new MySqlParameter("bearing", value.Bearing));
+            command.Parameters.Add(new MySqlParameter("path", value.PathData));
+            command.Parameters.Add(new MySqlParameter("color", value.Color));
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            command.ExecuteNonQuery();
+            
+            _conn.Close();
         }
     }
 }
